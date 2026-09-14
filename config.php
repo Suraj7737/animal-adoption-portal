9467551153f8b8305de02817b83a0996aa20b9d0
Suraj <bondharesuraj@gmail.com>
2026-09-15 00:30:41 +0530

    Change database credentials in config.php
    Updated database connection details for config.

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php - Database connection
-$db_host = 'sql103.infinityfree.com';        
-$db_user = 'if0_42712004';                 
-$db_pass = 'PMGOChampion';
-$db_name = 'if0_42712004_animal_adoption_db'; 
+$db_host = '.infinityfree.com';        
+$db_user = 'UserName';                 
+$db_pass = 'Password';
+$db_name = 'if00xx_animal_adoption_db'; 
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
